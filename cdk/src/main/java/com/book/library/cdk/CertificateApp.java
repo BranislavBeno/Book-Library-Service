67d91bfab78da40bf9d7a4816651187ca013a804
Branislav Beno <brano.beno@gmail.com>
2023-06-05 06:36:49 +0200

    Introduce CertificateApp

---
+++ cdk/src/main/java/com/book/library/cdk/CertificateApp.java
@@ -0,0 +1,32 @@
+package com.book.library.cdk;
+
+import com.book.library.cdk.construct.ApplicationEnvironment;
+import com.book.library.cdk.stack.CertificateStack;
+import com.book.library.cdk.util.CdkUtil;
+import com.book.library.cdk.util.Validations;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+
+public class CertificateApp {
+
+    public static void main(final String[] args) {
+        App app = new App();
+
+        String accountId = Validations.requireNonEmpty(app, "accountId");
+        String region = Validations.requireNonEmpty(app, "region");
+        String environmentName = Validations.requireNonEmpty(app, "environmentName");
+        String applicationName = Validations.requireNonEmpty(app, "applicationName");
+        String applicationDomain = Validations.requireNonEmpty(app, "applicationDomain");
+        String hostedZoneDomain = Validations.requireNonEmpty(app, "hostedZoneDomain");
+
+        Environment awsEnvironment = CdkUtil.makeEnv(accountId, region);
+
+        var appEnvironment = new ApplicationEnvironment(applicationName, environmentName);
+
+        String stackName =
+                "%s-certificate-%s".formatted(appEnvironment.applicationName(), appEnvironment.environmentName());
+        new CertificateStack(app, stackName, awsEnvironment, appEnvironment, applicationDomain, hostedZoneDomain);
+
+        app.synth();
+    }
+}
