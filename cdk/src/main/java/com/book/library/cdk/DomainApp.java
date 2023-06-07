8607b451516607885ebc0da79b29c6d576ac0f2e
Branislav Beno <brano.beno@gmail.com>
2023-06-07 06:29:08 +0200

    Introduce DomainApp

---
+++ cdk/src/main/java/com/book/library/cdk/DomainApp.java
@@ -0,0 +1,31 @@
+package com.book.library.cdk;
+
+import com.book.library.cdk.construct.ApplicationEnvironment;
+import com.book.library.cdk.stack.DomainStack;
+import com.book.library.cdk.util.CdkUtil;
+import com.book.library.cdk.util.Validations;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+
+public class DomainApp {
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        String accountId = Validations.requireNonEmpty(app, "accountId");
+        String region = Validations.requireNonEmpty(app, "region");
+        String environmentName = Validations.requireNonEmpty(app, "environmentName");
+        String applicationName = Validations.requireNonEmpty(app, "applicationName");
+        String applicationDomain = Validations.requireNonEmpty(app, "applicationDomain");
+        String hostedZoneDomain = Validations.requireNonEmpty(app, "hostedZoneDomain");
+
+        Environment awsEnvironment = CdkUtil.makeEnv(accountId, region);
+
+        var appEnvironment = new ApplicationEnvironment(applicationName, environmentName);
+
+        String stackName = "%s-domain-%s".formatted(appEnvironment.applicationName(), appEnvironment.environmentName());
+        new DomainStack(app, stackName, awsEnvironment, appEnvironment, hostedZoneDomain, applicationDomain);
+
+        app.synth();
+    }
+}
