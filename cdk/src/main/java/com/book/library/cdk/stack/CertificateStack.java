6fc1865ecc34befbd86b13ef5e0315bb50bfb92d
Branislav Beňo <57846939+BranislavBeno@users.noreply.github.com>
2023-06-08 11:32:44 +0200

    feat: Extend cdk module with CertificateApp and DomainApp (#40)
    * Introduce CertificateApp
    
    * Update CertificateStack
    
    * Add certificate validation
    
    * Add sslCertificateArn
    
    * Include sslCertificateArn into NetworkInputParameters
    
    * Introduce DomainApp
    
    * Change dockerImageTag
    
    ---------
    
    Co-authored-by: Branislav Beno <brano.beno@gmail.com>
    Co-authored-by: Branislav Beno <branislav.beno@siemens.com>

---
+++ cdk/src/main/java/com/book/library/cdk/stack/CertificateStack.java
@@ -0,0 +1,48 @@
+package com.book.library.cdk.stack;
+
+import com.book.library.cdk.construct.ApplicationEnvironment;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.certificatemanager.Certificate;
+import software.amazon.awscdk.services.certificatemanager.CertificateValidation;
+import software.amazon.awscdk.services.certificatemanager.ICertificate;
+import software.amazon.awscdk.services.route53.HostedZone;
+import software.amazon.awscdk.services.route53.HostedZoneProviderProps;
+import software.amazon.awscdk.services.route53.IHostedZone;
+import software.constructs.Construct;
+
+public class CertificateStack extends Stack {
+
+    public CertificateStack(
+            final Construct scope,
+            final String id,
+            final Environment awsEnvironment,
+            final ApplicationEnvironment applicationEnvironment,
+            final String applicationDomain,
+            final String hostedZoneDomain) {
+        super(
+                scope,
+                id,
+                StackProps.builder()
+                        .stackName(applicationEnvironment.prefix("Certificate"))
+                        .env(awsEnvironment)
+                        .build());
+
+        IHostedZone hostedZone = HostedZone.fromLookup(
+                this,
+                "HostedZone",
+                HostedZoneProviderProps.builder().domainName(hostedZoneDomain).build());
+
+        ICertificate websiteCertificate = Certificate.Builder.create(this, "WebsiteCertificate")
+                .domainName(applicationDomain)
+                .validation(CertificateValidation.fromDns(hostedZone))
+                .build();
+
+        new CfnOutput(
+                this,
+                "sslCertificateArn",
+                CfnOutputProps.builder()
+                        .exportName("sslCertificateArn")
+                        .value(websiteCertificate.getCertificateArn())
+                        .build());
+    }
+}
