f62cbbf66204f38cdafeed6105ef81cd08913ddd
Branislav Beno <brano.beno@gmail.com>
2023-07-18 10:28:52 +0200

    Introduce DatabaseApp

--- cdk/src/main/java/com/book/library/cdk/BootstrapApp.java
+++ cdk/src/main/java/com/book/library/cdk/BootstrapApp.java
@@ -7,11 +7,6 @@ import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
-/**
- * This is an empty app that we can use for bootstrapping the CDK with the "cdk bootstrap" command.
- * We could do this with other apps, but this would require us to enter all the parameters
- * for that app, which is uncool.
- */
 public class BootstrapApp {
 
     public static void main(final String[] args) {
