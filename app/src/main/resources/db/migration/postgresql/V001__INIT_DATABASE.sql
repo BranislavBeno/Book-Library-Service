3d1c7f3e0e12fe48b8ef0c91a7adf4f79b8fd72a
Branislav Beno <brano.beno@gmail.com>
2023-09-05 01:03:45 +0200

    Remove database initialization on application's start up

--- app/src/main/resources/db/migration/postgresql/V001__INIT_DATABASE.sql
+++ app/src/main/resources/db/migration/postgresql/V001__INIT_DATABASE.sql
@@ -8,11 +8,6 @@ CREATE TABLE reader (
    CONSTRAINT pk_reader PRIMARY KEY (id)
 );
 
-INSERT INTO reader (id, first_name, last_name) VALUES (1, 'Peter', 'Prvý');
-INSERT INTO reader (id, first_name, last_name) VALUES (2, 'Lukáš', 'Druhý');
-INSERT INTO reader (id, first_name, last_name) VALUES (3, 'Matej', 'Tretí');
-INSERT INTO reader (id, first_name, last_name) VALUES (4, 'Jozef', 'Štvrtý');
-
 -- Book
 CREATE SEQUENCE IF NOT EXISTS book_seq START WITH 20 INCREMENT BY 1;
 
@@ -22,13 +17,6 @@ CREATE TABLE book (
    author VARCHAR(255),
    CONSTRAINT pk_book PRIMARY KEY (id)
 );
-
-INSERT INTO book (id, name, author) VALUES (1, 'Starec a more', 'Ernest Hemingway');
-INSERT INTO book (id, name, author) VALUES (2, 'Rómeo a Júlia', 'William Shakespeare');
-INSERT INTO book (id, name, author) VALUES (3, 'Krvavé sonety', 'Pavol Országh Hviezdoslav');
-INSERT INTO book (id, name, author) VALUES (4, 'Hájniková žena', 'Pavol Országh Hviezdoslav');
-INSERT INTO book (id, name, author) VALUES (5, 'Hamlet', 'William Shakespeare');
-INSERT INTO book (id, name, author) VALUES (6, 'Živý bič', 'Milo Urban');
 
 -- Borrowed book
 CREATE SEQUENCE IF NOT EXISTS borrowed_book_seq START WITH 20 INCREMENT BY 1;
@@ -44,8 +32,3 @@ CREATE TABLE borrowed_book (
 ALTER TABLE borrowed_book ADD CONSTRAINT FK_BORROWED_BOOK_ON_BOOK FOREIGN KEY (book_id) REFERENCES book (id);
 
 ALTER TABLE borrowed_book ADD CONSTRAINT FK_BORROWED_BOOK_ON_READER FOREIGN KEY (reader_id) REFERENCES reader (id);
-
-INSERT INTO borrowed_book (id, borrowed_on, book_id, reader_id) VALUES (1, '2016-03-25', 1, 1);
-INSERT INTO borrowed_book (id, borrowed_on, book_id, reader_id) VALUES (2, '2018-06-16', 2, 2);
-INSERT INTO borrowed_book (id, borrowed_on, book_id, reader_id) VALUES (3, '2017-02-01', 3, 3);
-INSERT INTO borrowed_book (id, borrowed_on, book_id, reader_id) VALUES (4, '2009-10-25', 5, 4);
