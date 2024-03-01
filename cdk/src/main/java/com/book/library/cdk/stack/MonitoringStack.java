b3f751044de1369b8f4ab846631a8cea019c26ef
Branislav Beno <branislav.beno@siemens.com>
2024-03-01 19:14:28 +0100

    Create composite alarms

--- cdk/src/main/java/com/book/library/cdk/stack/MonitoringStack.java
+++ cdk/src/main/java/com/book/library/cdk/stack/MonitoringStack.java
@@ -1,11 +1,14 @@
 package com.book.library.cdk.stack;
 
 import com.book.library.cdk.construct.*;
-import com.book.library.cdk.util.CdkUtil;
 import java.util.Map;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.cloudwatch.*;
 import software.amazon.awscdk.services.cloudwatch.actions.SnsAction;
+import software.amazon.awscdk.services.logs.FilterPattern;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.MetricFilter;
+import software.amazon.awscdk.services.logs.MetricFilterProps;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.TopicProps;
 import software.amazon.awscdk.services.sns.subscriptions.EmailSubscription;
@@ -70,11 +73,78 @@ public class MonitoringStack extends Stack {
                         .actionsEnabled(true)
                         .build());
 
+        Alarm elb5xxAlarm = new Alarm(
+                this,
+                "elb5xxAlarm",
+                AlarmProps.builder()
+                        .alarmName("5xx-backend-alarm")
+                        .alarmDescription(
+                                "Alert on multiple HTTP 5xx ELB responses."
+                                        + "See the runbook for a diagnosis and mitigation hints: https://github.com/stratospheric-dev/stratospheric/blob/main/docs/runbooks/elb5xxAlarm.md")
+                        .metric(new Metric(MetricProps.builder()
+                                .namespace("AWS/ApplicationELB")
+                                .metricName("HTTPCode_ELB_5XX_Count")
+                                .dimensionsMap(Map.of("LoadBalancer", loadBalancerName))
+                                .region(awsEnvironment.getRegion())
+                                .period(Duration.minutes(5))
+                                .statistic("sum")
+                                .build()))
+                        .treatMissingData(TreatMissingData.NOT_BREACHING)
+                        .comparisonOperator(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD)
+                        .evaluationPeriods(3)
+                        .datapointsToAlarm(3)
+                        .threshold(5)
+                        .actionsEnabled(false)
+                        .build());
+
+        MetricFilter errorLogsMetricFilter = new MetricFilter(
+                this,
+                "errorLogsMetricFilter",
+                MetricFilterProps.builder()
+                        .metricName("backend-error-logs")
+                        .metricNamespace("b-l-s")
+                        .metricValue("1")
+                        .defaultValue(0)
+                        .logGroup(LogGroup.fromLogGroupName(this, "applicationLogGroup", appEnvironment + "-logs"))
+                        .filterPattern(FilterPattern.stringValue("$.level", "=", "ERROR")) // { $.level = "ERROR" }
+                        .build());
+
+        Metric errorLogsMetric = errorLogsMetricFilter.metric(MetricOptions.builder()
+                .period(Duration.minutes(5))
+                .statistic("sum")
+                .region(awsEnvironment.getRegion())
+                .build());
+
+        Alarm errorLogsAlarm = errorLogsMetric.createAlarm(
+                this,
+                "errorLogsAlarm",
+                CreateAlarmOptions.builder()
+                        .alarmName("backend-error-logs-alarm")
+                        .alarmDescription("Alert on multiple ERROR backend logs")
+                        .treatMissingData(TreatMissingData.NOT_BREACHING)
+                        .comparisonOperator(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD)
+                        .evaluationPeriods(3)
+                        .threshold(5)
+                        .actionsEnabled(false)
+                        .build());
+
+        CompositeAlarm compositeAlarm = new CompositeAlarm(
+                this,
+                "basicCompositeAlarm",
+                CompositeAlarmProps.builder()
+                        .actionsEnabled(true)
+                        .compositeAlarmName("backend-api-failure")
+                        .alarmDescription("Showcasing a Composite Alarm")
+                        .alarmRule(AlarmRule.allOf(
+                                AlarmRule.fromAlarm(elb5xxAlarm, AlarmState.ALARM),
+                                AlarmRule.fromAlarm(errorLogsAlarm, AlarmState.ALARM)))
+                        .build());
+
         Topic snsAlarmingTopic = new Topic(
                 this,
                 "snsAlarmingTopic",
                 TopicProps.builder()
-                        .topicName(CdkUtil.createStackName("alarming-topic", appEnvironment))
+                        .topicName(appEnvironment + "-alarming-topic")
                         .displayName("SNS Topic to further route Amazon CloudWatch Alarms")
                         .build());
 
@@ -82,5 +152,6 @@ public class MonitoringStack extends Stack {
                 EmailSubscription.Builder.create(confirmationEmail).build());
 
         elbSlowResponseTimeAlarm.addAlarmAction(new SnsAction(snsAlarmingTopic));
+        compositeAlarm.addAlarmAction(new SnsAction(snsAlarmingTopic));
     }
 }
